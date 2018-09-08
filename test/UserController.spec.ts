582c0961ac4f0e092b61e8943d2c8917de058095
Kunjan343 <kunjanpatelrock@gmail.com>
2018-09-08 13:23:06 +0530

    config build and update travis ci

--- test/UserController.spec.ts
+++ test/UserController.spec.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import * as test from 'tape';
 import { UserRoute } from '../src/routes/UserRoute';
 
-test('addressController', (t) => {
+test('userRoute', (t) => {
     t.plan(1);
 
     const route = new UserRoute(null);
