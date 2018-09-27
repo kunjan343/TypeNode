5835ca4e35eda6922f2f00091b3e2c83ebfc8274
kunjan343 <kunjanpatelrock@gmail.com>
2018-09-27 12:46:18 +0530

    add application messages constants

---
+++ src/constants/message/user.message.ts
@@ -0,0 +1,17 @@
+export const USER_MESSAGE = {
+    SUCCESS: {
+        CREATE: 'Successfully created',
+        UPDATE: 'Successfully updated',
+        DELETE: 'Successfully deleted'
+    },
+    ERROR: {
+        EMPTY: {
+            USERNAME: 'Username required',
+            PASSWORD: 'Password required'
+        },
+        INVALID: {
+            EMAIL: 'Email format is invalid'
+        },
+        EXCEPTION: 'Something went wrong, please try again'
+    }
+};
